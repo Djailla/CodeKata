19f2fabf076a9121362fc8695a2645e1e93802b5
Djailla <bastien.vallet@gmail.com>
2015-10-27 16:16:59 +0100

    Add informations

--- 15/diversion.py
+++ 15/diversion.py
@@ -1,3 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+Working on exercice :
+
+http://codekata.com/kata/kata15-a-diversion/
+
+The answer for the last question is :
+
+n = (n-1) + (n-2)
+"""
+
 import itertools
 
 
@@ -11,7 +24,6 @@ def get_items(item_size):
             my_list.append(''.join(item))
 
     print "For %d element, the list size is %d" % (item_size, len(my_list))
-    # print '\n'.join(my_list)
 
-for n in [2, 3, 4, 5, 10, 20]:
+for n in xrange(1, 11):
     get_items(n)
