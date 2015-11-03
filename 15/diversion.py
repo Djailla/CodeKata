4810314f83cb0e36b7d10c2fbca1be81c5a6af29
Djailla <bastien.vallet@gmail.com>
2015-11-03 16:41:30 +0100

    Add new functions to test some other ways

--- 15/diversion.py
+++ 15/diversion.py
@@ -9,21 +9,76 @@ http://codekata.com/kata/kata15-a-diversion/
 The answer for the last question is :
 
 n = (n-1) + (n-2)
+
+n - 1 => All starting with 0
+n - 2 => All starting with 10
 """
 
 import itertools
 
 
-def get_items(item_size):
-    allorderings = itertools.product(['0', '1'], repeat=item_size)
+def get_items1(item_size):
+    """Using string compare we can look for the pattern in the string"""
+    if item_size < 1:
+        return 0
 
-    my_list = []
+    item_count = sum(1 for item in
+                     itertools.product(['0', '1'], repeat=item_size)
+                     if not '11' in ''.join(item)
+                     )
+    return item_count
 
-    for item in allorderings:
-        if not '11' in ''.join(item):
-            my_list.append(''.join(item))
 
-    print "For %d element, the list size is %d" % (item_size, len(my_list))
+def get_items2(item_size):
+    """Trying to use Boolean instead of strings"""
+    if item_size < 1:
+        return 0
 
-for n in xrange(1, 11):
-    get_items(n)
+    range_size = range(item_size - 1)
+
+    item_count = sum(1 for item in
+                     itertools.product([False, True], repeat=item_size)
+                     if not sum((item[i] and item[i+1] and 1 or 0) for i in range_size)
+                     )
+    return item_count
+
+
+def get_items3(item_size):
+    """Using the real expression to calculate final value"""
+    if item_size < 1:
+        return 0
+
+    item_list = [1, 2, 3]
+
+    if item_size < len(item_list):
+        return item_list[item_size]
+
+    for n in range(item_size - 2):
+        item_list.append(item_list[-1] + item_list[-2])
+
+    return item_list[-1]
+
+
+def do_it(n):
+    for x in range(100):
+        get_items1(n)
+
+
+def do_it2(n):
+    for x in range(100):
+        get_items2(n)
+
+
+def do_it3(n):
+    for x in range(100):
+        get_items3(n)
+
+
+for n in xrange(-3, 20):
+    print "get_items1 : For %d element, the list size is %d" % (n, get_items1(n))
+    print "get_items2 : For %d element, the list size is %d" % (n, get_items2(n))
+    print "get_items3 : For %d element, the list size is %d" % (n, get_items3(n))
+
+# import profile
+# profile.run('do_it(10)')
+# profile.run('do_it3(10)')
