38caa49e9e3f6b80ce8ed01bd9fc6ec91cdbd9a5
Djailla <bastien.vallet@gmail.com>
2015-11-09 13:56:16 +0100

    Add monkey testing

--- custom/180.py
+++ custom/180.py
@@ -17,6 +17,10 @@ i/p: 123
 o/p: False
 """
 
+import random
+
+ALL_DIGIT = range(0, 10)
+
 MATCH_SIDE = {
     0: 0,
     1: 1,
@@ -24,8 +28,12 @@ MATCH_SIDE = {
     8: 8,
     9: 6,
 }
-
+MATCH_EXTREMITY = list(set(MATCH_SIDE.keys()) - set([0]))
 MATCH_MIDDLE = [0, 1, 8]
+
+BAD_SIDE = list(set(ALL_DIGIT) - set(MATCH_SIDE.keys()))
+BAD_EXTREMITY = list(set(BAD_SIDE) - set(MATCH_EXTREMITY))
+BAD_MIDDLE = list(set(ALL_DIGIT) - set(MATCH_MIDDLE))
 
 
 def test_180_value(test_value):
@@ -53,9 +61,52 @@ def test_180_value(test_value):
         return False
 
 
-print test_180_value(180)
-print test_180_value(69)
-print test_180_value(916)
-print test_180_value(11111)
-print test_180_value(96196)
-print test_180_value(8181818)
+assert(test_180_value(180) is False)
+assert(test_180_value(69) is True)
+assert(test_180_value(916) is True)
+assert(test_180_value(11111) is True)
+assert(test_180_value(8181818) is True)
+
+# Monkey testing
+for i in range(100):
+    number_list = []
+    number_len = random.randint(1, 10)
+    print "Genarate a number of %d size" % number_len
+
+    # if len is odd, insert a correct middle value
+    if number_len % 2 == 1:
+        number_list.append(random.choice(MATCH_MIDDLE))
+        number_len -= 1
+
+    # Then generate the extra parameters
+    for k in range(number_len / 2):
+        if k == (number_len / 2) - 1:
+            left = random.choice(MATCH_EXTREMITY)
+        else:
+            left = random.choice(MATCH_SIDE.keys())
+        right = MATCH_SIDE[left]
+        number_list.append(right)
+        number_list.insert(0, left)
+
+    good_number = int(''.join(map(str, number_list)))
+
+    print "### GOOD NUMBER %d" % good_number
+    assert(test_180_value(good_number) is True)
+
+    # Insert a bad parameter anywhere
+    bad_list = list(number_list)
+    bad_list.insert(random.randint(0, len(number_list) / 2), random.choice(BAD_SIDE))
+    bad_number = int(''.join(map(str, bad_list)))
+
+    print "### BAD NUMBER %d" % bad_number
+    assert(test_180_value(bad_number) is False)
+
+    bad_list = list(number_list)
+    if len(bad_list) % 2 == 1:
+        bad_list[len(bad_list) / 2] = random.choice(BAD_MIDDLE)
+    else:
+        bad_list.insert(random.randint(0, len(bad_list) / 2), random.choice(BAD_SIDE))
+    bad_number = int(''.join(map(str, bad_list)))
+
+    print "### BAD NUMBER %d" % bad_number
+    assert(test_180_value(bad_number) is False)
